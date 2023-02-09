a78fed723816a43e41ca15b063e021c9c3a8ad61
ParasRajput <pr8101999@gmail.com>
2023-02-09 20:36:55 +0530

    Minor cleaning of code

--- test.js
+++ test.js
@@ -1,36 +1,34 @@
-const arr = [[2,4,6] , [0,4,8] , [0,3,6],[1,4,7] , [2,5,8],[0,1,2],[3,4,5],[6,7,8]];
-const testarr= [];
+const Winingconditions = [[2,4,6] , [0,4,8] , [0,3,6],[1,4,7] , [2,5,8],[0,1,2],[3,4,5],[6,7,8]];
+const playerXmoves= [];
 let player = "X";
-const test2arr = [];
-let winnerElem = document.getElementById("winnerDeclartion");
+const playerOmoves = [];
+let winnerDecElement = document.getElementById("winnerDeclartion");
 
 function newclick(event){
     let targetelement = event.target;
 
     if(player == "X"){
         targetelement.removeAttribute("onclick");
-        testarr.push(Number.parseInt( targetelement.getAttribute('value')));
+        playerXmoves.push(Number.parseInt( targetelement.getAttribute('value')));
         targetelement.innerHTML = "X";
-        console.log(player , testarr);
-        for(let i=0 ; i<=arr.length-1 ; i++){
-            if(arr[i].every(elem => testarr.includes(elem))){
+        for(let i=0 ; i<=Winingconditions.length-1 ; i++){
+            if(Winingconditions[i].every(elem => playerXmoves.includes(elem))){
                 console.log("Player X won");
                 disablerepeated(targetelement);
-                winnerElem.innerHTML = "<h1>Player X Won</h1>"
+                winnerDecElement.innerHTML = "<h1>Player X Won</h1>"
             }
         }
         player = "O"
     }
     else{
-        test2arr.push(Number.parseInt( targetelement.getAttribute('value')));
+        playerOmoves.push(Number.parseInt( targetelement.getAttribute('value')));
         targetelement.innerHTML = "O";
         targetelement.removeAttribute("onclick");
-        console.log(player , test2arr);
-        for(let i=0 ; i<=arr.length-1 ; i++){
-            if(arr[i].every(elem => test2arr.includes(elem))){
+        for(let i=0 ; i<=Winingconditions.length-1 ; i++){
+            if(Winingconditions[i].every(elem => playerOmoves.includes(elem))){
                 console.log("player O Won");
                 disablerepeated(targetelement);
-                winnerElem.innerHTML = "<h1>Player O Won</h1>"
+                winnerDecElement.innerHTML = "<h1>Player O Won</h1>"
             }
         }
         player = "X"
